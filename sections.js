b16883eeb9029e92b62f3bda4848dc46c8addd56
Daniel Hartgrove <hartgrovedaniel3@outlook.com>
2025-02-20 22:53:48 +0000

    update styles to look less goofy

--- sections.js
+++ sections.js
@@ -6,7 +6,7 @@ buttons[0].classList.add("activeTab");
 
 function showSection(sectionIndex) {
   for (let i = 0; i < sections.length; i++) {
-    sections[i].style.display = i === sectionIndex ? "block" : "none";
+    sections[i].style.display = i === sectionIndex ? "flex" : "none";
 
     if (i === sectionIndex) {
         sections[i].scrollIntoView({ behavior: 'instant' });
@@ -17,10 +17,15 @@ function showSection(sectionIndex) {
     buttons[i].classList.remove("activeTab");
   }
   buttons[sectionIndex].classList.add("activeTab");
-}
+};
+
+function toTop(){
+  window.scrollTo(0, 0);
+};
 
 window.onload = function() {
-    sections[0].style.display = "block";
+    sections[0].style.display = "flex";
     buttons[0].classList.add("activeTab");
   };
   
+
